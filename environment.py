softmax decision implemented, interactive plotting fixed, other minor changes

environment.py
@@ -64,7 +64,7 @@ class PaperRaceEnv:
         if np.array_equal(spd_new, [0,0]):
             end = True
 
-        if self.crosses_finish_line(pos_old, spd_new, self.segm_list[self.segm][0:2], self.segm_list[self.segm][2:]):
+        if not end and self.crosses_finish_line(pos_old, spd_new, self.segm_list[self.segm][0:2], self.segm_list[self.segm][2:]):
             reward = 1000
             self.segm += 1
             if self.segm > len(self.segm_list):
@@ -104,7 +104,7 @@ class PaperRaceEnv:
         # Annak eldontese hogy akkor az egyenesek metszespontja az most a
         # szakaszokon belulre esik-e: Ha mindket t, t1 es t2 is kisebb mint 1 és
         # nagyobb mint 0
-        celba = (0<=t1) and (t1<=1) and (0<=t2) and (t2<=1)
+        celba = 0 <= t1 <= 1 and 0 <= t2 <= 1
         if not(celba):
             t2 = 0
         return celba
@@ -146,5 +146,3 @@ class PaperRaceEnv:
         sizeGGY = np.shape(self.gg_pic)[0]
         data[4] = data_orig[4] / sizeGGX # Az action vektor átlaga 0 körülinek feltételezhető, így csak a méretét kell normalizálni
         data[5] = data_orig[5] / sizeGGY
-
-

main.py
@@ -8,6 +8,12 @@ import random
 
 from environment import PaperRaceEnv
 
+def softmax(x):
+    exp = np.exp(x)
+    s = sum(exp)
+    return exp / s
+
+
 plt.ion()
 plt.show()
 
@@ -37,7 +43,8 @@ exp_memory = deque(maxlen=mem_size)
 
 episodes = 10000
 
-for _ in range(episodes):
+for ep in range(episodes):
+    print(ep)
     plt.clf()
     env.draw_track()
     v = np.array([1, 0])
@@ -45,11 +52,25 @@ for _ in range(episodes):
     reward = 0
     end = False
     while not end:
+
+        #e-greedy
         if random.random() < explore:
             action = random.randint(1, 9)
         else:
             qs = [qn.predict(np.array([np.concatenate((pos, v, env.gg_action(act)))]))[0] for act in range(1, 10)]
             action = np.argmax(qs) + 1
+
+        # #softmax
+        # qs = [qn.predict(np.array([np.concatenate((pos, v, env.gg_action(act)))]))[0] for act in range(1, 10)]
+        # sm = softmax(qs)
+        # cs = np.cumsum(sm)
+        # action = 1
+        # rand = random.random()
+        # for i in range(len(cs)):
+        #     if rand > cs[i]:
+        #         action = i+1
+        #     else:
+        #         break
 
         action = env.gg_action(action)
         v_new, pos_new, reward, end = env.step(action, v, pos, True)
@@ -83,4 +104,5 @@ for _ in range(episodes):
         v = v_new
         pos = pos_new
 
-        plt.draw()
+    plt.pause(0.001)
+    plt.draw()